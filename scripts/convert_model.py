d990a7cafafdeb02e14e2a416d093f7e39b87832
jh.liao <jh.liao@outlook.com>
2025-05-28 17:13:32 +0800

    update MiniMindConfig parameters for model conversion

--- scripts/convert_model.py
+++ scripts/convert_model.py
@@ -62,7 +62,7 @@ def convert_transformers2torch(transformers_path, torch_path):
 
 
 if __name__ == '__main__':
-    lm_config = MiniMindConfig(hidden_size=768, num_hidden_layers=16, max_seq_len=8192, use_moe=False)
+    lm_config = MiniMindConfig(hidden_size=512, num_hidden_layers=8, max_seq_len=512, use_moe=False, vocab_size=3200)
 
     torch_path = f"../out/full_sft_{lm_config.hidden_size}{'_moe' if lm_config.use_moe else ''}.pth"
 
